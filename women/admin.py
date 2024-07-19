2eabd269e289b6bc4d4f3c0aec788720fcb60f29
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2024-07-19 17:17:26 +0300

    Admin panel adjustments

--- women/admin.py
+++ women/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from women.models import Women
+from women.models import Women, Category
 
-admin.site.register(Women)
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
+    list_display_links = ('id', 'title')
+    ordering = ['time_create', 'title']
+    list_editable = ('is_published',)
+    list_per_page = 5
+
+
+@admin.register(Category)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+# admin.site.register(Women, WomenAdmin)
