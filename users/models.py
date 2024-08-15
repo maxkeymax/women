94c3575b86e11b2a91ab7a5675ec1ce69e25b052
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2024-08-15 18:59:56 +0300

    Users model used

--- users/models.py
+++ users/models.py
@@ -5,4 +5,3 @@ from django.db import models
 class User(AbstractUser):
     photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True, null=True, verbose_name="Фотография")
     date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата рождения")
-
